60354d0bbe67e09afecac96f04f755fd423187d2
donks666-cyber <xunpeijie@gmail.com>
2026-06-01 03:13:55 -0400

    🤏A Little Bit of Repair

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-sudo apt update && sudo apt upgrade -y
-
 sudo apt-get install -y wget curl git thunar
 sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
@@ -30,9 +27,13 @@ cd ../..
 pip3 install pywal
 
 mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3blocks
 mkdir -p ~/.config/compton
 mkdir -p ~/.config/rofi
 mkdir -p ~/.config/alacritty
+git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks
+# Override bandwidth script with fixed version (shows 0 on first run instead of blank)
+cp .config/i3blocks/bandwidth/bandwidth ~/.config/i3blocks/bandwidth/bandwidth
 cp .config/i3/config ~/.config/i3/config
 cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
@@ -46,3 +47,12 @@ echo "Done! Grab some wallpaper and run pywal -i filename to set your color sche
 echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo apt-get install -y autojump
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+sed -i 's/^plugins=(git)$/plugins=(git autojump zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+sed -i 's/^# DISABLE_MAGIC_FUNCTIONS=true/DISABLE_MAGIC_FUNCTIONS=true/' ~/.zshrc
+grep -q "zstyle ':omz:update' mode disabled" ~/.zshrc || echo "zstyle ':omz:update' mode disabled" >> ~/.zshrc
